566c8ebc1d96d0284fb0c7134b1aea1ed734a835
anduser <huraccane@gmail.com>
2019-07-10 10:55:42 +0300

    Тестовые задания 3 + создание класса и обеъектов с параметрами + метод

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,26 @@
 public class Test {
     public static void main(String[] args) {
         Auto car1 = new Auto();
-        car1.power = 150;
-        car1.name = "Toyota Rav4";
-        System.out.println(car1.name + ": " + car1.power + " PH");
+        car1.name = "BMW X3";
+        car1.price = 40000;
+        car1.cur = "S";
+        car1.information();
 
         Auto car2 = new Auto();
-        car2.power = 245;
-        car2.name = "BMW X3";
-        System.out.println(car2.name + ": " + car2.power + " PH");
+        car2.name = "Audi Q5";
+        car2.price = 38000;
+        car2.cur = "E";
+        car2.information();
     }
 }
 
 class Auto {
-        int power;
-        String name;
+    int price;
+    String name;
+    String cur;
+
+    void information() {
+        System.out.println(name + " = " + price + cur);
     }
+}
+
